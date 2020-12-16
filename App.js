a9fdc87ea40262f0f785c246c3aa5f7ef70dcfcc
prmstrgt <pramestiregitaa@gmail.com>
2020-12-16 07:41:45 +0700

    add splash screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Root} from 'native-base';
+import RNBootSplash from 'react-native-bootsplash';
 
 import Main from './src/screens/Main';
 import store from './src/redux/store';
 
 export default function App() {
+  useEffect(() => {
+    RNBootSplash.show({});
+  }, []);
+
   return (
     <Provider store={store().store}>
       <PersistGate loading={null} persistor={store().persistor}>
